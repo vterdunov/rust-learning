64eb85eccf2af93c0be6dbcb405e3bfe1c20ac2a
vterdunov <mail2slick@gmail.com>
2018-11-03 17:19:44 +0400

    ch 5.3 methods

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,28 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.length * self.width
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
 }
 
 fn main() {
-    let r = Rectangle {
+    let r1 = Rectangle {
         length: 50,
         width: 30,
     };
+    let r2 = Rectangle {
+        length: 40,
+        width: 10,
+    };
 
-    println!("area {:#?}", r.area());
+    let r3 = Rectangle {
+        length: 45,
+        width: 60,
+    };
+
+    println!("area rect1 {:#?}", r1.area());
+    println!("can rect1 hold rect2 {}", r1.can_hold(&r2));
+    println!("can rect1 hold rect3 {}", r1.can_hold(&r3));
 }
