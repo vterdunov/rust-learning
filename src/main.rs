1c370bf7b462e919b80329bfc48d12acb5d12cc4
vterdunov <mail2slick@gmail.com>
2018-11-03 17:12:38 +0400

    ch 5.3 methods

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,17 @@ struct Rectangle {
     width: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+}
+
 fn main() {
     let r = Rectangle {
         length: 50,
         width: 30,
     };
 
-    println!("rectangle {:?}", r);
+    println!("area {:#?}", r.area());
 }
