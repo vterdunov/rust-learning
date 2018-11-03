ccea31556ac622e0c8a86d6f59d8e70a3f31c6b0
vterdunov <mail2slick@gmail.com>
2018-11-03 16:08:46 +0400

    ch 4.2. references and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 fn main() {
     let s1 = String::from("Hello");
 
-    let (s2, len) = calculate_length(s1);
+    let len = calculate_length(&s1);
 
-    println!("{}{}", s2, len);
+    println!("{}{}", s1, len);
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len();
-
-    (s, length)
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
