fdf724b302c63f4118c8bfa67fab91d3ef8d40f4
vterdunov <mail2slick@gmail.com>
2018-11-05 16:42:04 +0400

    ch 9.3 panic or not to panic

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,57 @@
-#[derive(Debug)]
-struct Rectangle {
-    length: u32,
-    width: u32,
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+pub struct Guess {
+    value: u32,
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.length * self.width
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.length > other.length && self.width > other.width
-    }
-
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            length: size,
-            width: size,
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Value must be between 1 and 100, got {}", value);
         }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
     }
 }
 
 fn main() {
-    let r1 = Rectangle {
-        length: 50,
-        width: 30,
-    };
-    let r2 = Rectangle {
-        length: 40,
-        width: 10,
-    };
+    println!("Guess the number!");
 
-    let r3 = Rectangle {
-        length: 45,
-        width: 60,
-    };
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("area rect1 {:#?}", r1.area());
-    println!("can rect1 hold rect2 {}", r1.can_hold(&r2));
-    println!("can rect1 hold rect3 {}", r1.can_hold(&r3));
+    loop {
+        println!("Please input your number");
+        let mut guess = String::new();
 
-    let r4 = Rectangle::square(50);
-    println!("area rect1 {:#?}", r4.area());
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let guess_item: Guess = Guess::new(guess as u32);
+        let guess: u32 = guess_item.value();
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
