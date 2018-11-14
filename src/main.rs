708655841e60c1659c11862f02696bf569b8aef9
vterdunov <mail2slick@gmail.com>
2018-11-14 21:46:28 +0400

    ch10-02 traits

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,49 @@
-extern crate rand;
-
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
-
-pub struct Guess {
-    value: u32,
+pub trait Summarizible {
+    fn summary(&self) -> String;
 }
 
-impl Guess {
-    pub fn new(value: u32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Value must be between 1 and 100, got {}", value);
-        }
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
 
-        Guess { value }
+impl Summarizible for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{} {}", self.author, self.content)
     }
+}
 
-    pub fn value(&self) -> u32 {
-        self.value
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizible for Tweet {
+    fn summary(&self) -> String {
+        format!("{} {}", self.username, self.content)
     }
 }
 
 fn main() {
-    println!("Guess the number!");
+    let article = NewsArticle {
+        headline: String::from("headline"),
+        location: String::from("headline"),
+        author: String::from("author"),
+        content: String::from("content"),
+    };
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("{}", article.summary());
 
-    loop {
-        println!("Please input your number");
-        let mut guess = String::new();
+    let tweet = Tweet {
+        username: String::from("username"),
+        content: String::from("content"),
+        reply: true,
+        retweet: false,
+    };
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        let guess_item: Guess = Guess::new(guess as u32);
-        let guess: u32 = guess_item.value();
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    println!("{}", tweet.summary());
 }
